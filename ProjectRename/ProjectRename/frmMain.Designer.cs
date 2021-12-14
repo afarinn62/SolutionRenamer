316136bc5ddc1adddc6f1105277b90661b088b63
Nafarin <Nafarin@NAFARIN>
2021-12-14 13:56:13 +0330

    Add project files.

---
+++ ProjectRename/ProjectRename/frmMain.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ProjectRename
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(394, 65);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(116, 13);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(353, 20);
+            this.txtFrom.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Location = new System.Drawing.Point(37, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "From ";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(116, 39);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(353, 20);
+            this.txtTo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "To";
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(40, 64);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(348, 23);
+            this.progress.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 103);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Solution Renamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progress;
+    }
+}
+
---
+++ ProjectRename/ProjectRename/frmMain.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectRename
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtFrom.Text) || String.IsNullOrEmpty(txtTo.Text))
+            {
+                MessageBox.Show("Please Set From and To Textes");
+            }
+            else
+            {
+                FolderBrowserDialog fd = new FolderBrowserDialog();
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    string from = txtFrom.Text; 
+                    string to = txtTo.Text;
+                    string selectedFolder = fd.SelectedPath;
+                    string parent  = Directory.GetParent(selectedFolder).ToString();
+                    string newFolder = parent + $"\\New-For-{to}";
+                    await CreateDirectoryStructure(newFolder, selectedFolder, from, to);
+
+                    await CopyAndReplace(newFolder, selectedFolder, from, to);
+                    MessageBox.Show("All Done.");
+                }
+            }
+
+        }
+
+        string[] GetAllDirectory(string path)
+        {
+            string[] directories = System.IO.Directory.GetDirectories(path, "*", System.IO.SearchOption.AllDirectories);
+            return directories;
+        }
+
+        string[] GetAllFiles(string path)
+        {
+            string[] files = System.IO.Directory.GetFiles(path, "*", System.IO.SearchOption.AllDirectories);
+            return files;
+        }
+
+        async Task CreateDirectoryStructure(string newPath, string basePath, string from, string to)
+        {
+            Directory.CreateDirectory(newPath);
+
+            await Task.Factory.StartNew(() =>
+            {
+                var directorieAddress = GetAllDirectory(basePath);
+                Invoke(new Action(() =>
+                {
+                    progress.Maximum = directorieAddress.Length;
+                    progress.Value = 0;
+                }));
+
+                foreach (var item in directorieAddress)
+                {
+                    Invoke(new Action(() => progress.Value += 1));
+                    var newItem = item.Replace(basePath, newPath);
+                    newItem = newItem.Replace(from, to);
+                    Directory.CreateDirectory(newItem);
+                }
+            });
+
+
+        }
+
+
+        async Task CopyAndReplace(string newPath, string basePath, string from, string to)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                var files = GetAllFiles(basePath);
+
+                Invoke(new Action(() =>
+                {
+                    progress.Maximum = files.Length;
+                    progress.Value = 0;
+                })) ;
+                foreach (var item in files)
+                {
+                    Invoke(new Action(() => progress.Value += 1));
+                    var newItem = item.Replace(basePath, newPath);
+                    newItem = newItem.Replace(from, to);
+                    String content = File.ReadAllText(item);
+                    content = content.Replace(from, to);
+                    File.WriteAllText(newItem, content);
+                }
+            });
+        }
+    }
+}
