4b999845c34077bfc79bdc2bdad67fe6c17625ab
nafarin <nafarin@NAFARIN>
2022-07-03 14:32:05 +0430

    add solution renamer

--- ProjectRenamer/frmMain.Designer.cs
+++ ProjectRenamer/frmMain.Designer.cs
@@ -35,13 +35,19 @@
             this.txtTo = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.progress = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSolutionTo = new System.Windows.Forms.TextBox();
+            this.txtSolutionFrom = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSelectFolder
             // 
-            this.btnSelectFolder.Location = new System.Drawing.Point(394, 65);
+            this.btnSelectFolder.Location = new System.Drawing.Point(42, 12);
             this.btnSelectFolder.Name = "btnSelectFolder";
-            this.btnSelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFolder.Size = new System.Drawing.Size(65, 23);
             this.btnSelectFolder.TabIndex = 0;
             this.btnSelectFolder.Text = "Select Folder";
             this.btnSelectFolder.UseVisualStyleBackColor = true;
@@ -49,7 +55,7 @@
             // 
             // txtFrom
             // 
-            this.txtFrom.Location = new System.Drawing.Point(116, 13);
+            this.txtFrom.Location = new System.Drawing.Point(118, 41);
             this.txtFrom.Name = "txtFrom";
             this.txtFrom.Size = new System.Drawing.Size(353, 20);
             this.txtFrom.TabIndex = 1;
@@ -58,7 +64,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.label1.Location = new System.Drawing.Point(37, 16);
+            this.label1.Location = new System.Drawing.Point(39, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 2;
@@ -66,7 +72,7 @@
             // 
             // txtTo
             // 
-            this.txtTo.Location = new System.Drawing.Point(116, 39);
+            this.txtTo.Location = new System.Drawing.Point(118, 67);
             this.txtTo.Name = "txtTo";
             this.txtTo.Size = new System.Drawing.Size(353, 20);
             this.txtTo.TabIndex = 1;
@@ -74,7 +80,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(37, 42);
+            this.label2.Location = new System.Drawing.Point(39, 70);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(20, 13);
             this.label2.TabIndex = 2;
@@ -82,21 +88,78 @@
             // 
             // progress
             // 
-            this.progress.Location = new System.Drawing.Point(40, 64);
+            this.progress.Location = new System.Drawing.Point(42, 145);
             this.progress.Name = "progress";
-            this.progress.Size = new System.Drawing.Size(348, 23);
+            this.progress.Size = new System.Drawing.Size(429, 23);
             this.progress.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Location = new System.Drawing.Point(39, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Solution Folder name from";
+            // 
+            // txtSolutionTo
+            // 
+            this.txtSolutionTo.Location = new System.Drawing.Point(262, 119);
+            this.txtSolutionTo.Name = "txtSolutionTo";
+            this.txtSolutionTo.Size = new System.Drawing.Size(209, 20);
+            this.txtSolutionTo.TabIndex = 4;
+            // 
+            // txtSolutionFrom
+            // 
+            this.txtSolutionFrom.Location = new System.Drawing.Point(262, 93);
+            this.txtSolutionFrom.Name = "txtSolutionFrom";
+            this.txtSolutionFrom.Size = new System.Drawing.Size(209, 20);
+            this.txtSolutionFrom.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Location = new System.Drawing.Point(39, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Solution Folder name to";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(118, 14);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(353, 20);
+            this.txtPath.TabIndex = 9;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(225, 174);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(65, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(511, 103);
+            this.ClientSize = new System.Drawing.Size(509, 212);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSolutionTo);
+            this.Controls.Add(this.txtSolutionFrom);
             this.Controls.Add(this.progress);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtTo);
             this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.btnSelectFolder);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmMain";
@@ -115,6 +178,12 @@
         private System.Windows.Forms.TextBox txtTo;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSolutionTo;
+        private System.Windows.Forms.TextBox txtSolutionFrom;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
--- ProjectRenamer/frmMain.cs
+++ ProjectRenamer/frmMain.cs
@@ -20,36 +20,44 @@ namespace ProjectRename
 
         private async void btnSelectFolder_Click(object sender, EventArgs e)
         {
-            try
+            FolderBrowserDialog fd = new FolderBrowserDialog();
+            if (fd.ShowDialog() == DialogResult.OK)
             {
-                if (String.IsNullOrEmpty(txtFrom.Text) || String.IsNullOrEmpty(txtTo.Text))
+                txtPath.Text = fd.SelectedPath;
+                var slnFiles = System.IO.Directory.GetFiles(txtPath.Text, "*.sln", System.IO.SearchOption.TopDirectoryOnly).ToList();
+                if (slnFiles.Count > 0)
                 {
-                    MessageBox.Show("Please Set From and To Textes");
+                    txtSolutionFrom.Text = Path.GetFileNameWithoutExtension(slnFiles[0]);
                 }
-                else
-                {
-                    FolderBrowserDialog fd = new FolderBrowserDialog();
-                    if (fd.ShowDialog() == DialogResult.OK)
-                    {
-                        string from = txtFrom.Text;
-                        string to = txtTo.Text;
-                        string selectedFolder = fd.SelectedPath;
-                        string parent = Directory.GetParent(selectedFolder).ToString();
-                        string newFolder = parent + $"\\New-For-{to}";
-                        await CreateDirectoryStructure(newFolder, selectedFolder, from, to);
 
-                        await CopyAndReplace(newFolder, selectedFolder, from, to);
-                        MessageBox.Show("All Done.");
-                    }
+                var directories = System.IO.Directory.GetDirectories(txtPath.Text, "*", System.IO.SearchOption.TopDirectoryOnly).Select(c => new DirectoryInfo(c).Name).ToList();
+                if (directories.Count > 0)
+                {
+                    txtFrom.Text = GetCommonText(directories);
                 }
             }
-            catch (Exception ex)
+        }
+
+        string GetCommonText(List<string> items)
+        {
+            var parsedItems = items.Select(c => new { item = c, splitedItems = c.Split('.').ToList() });
+            var items0 = parsedItems.Where(c => c.splitedItems.Count > 0).Select(c => c.splitedItems[0]).ToList();
+            var items1 = parsedItems.Where(c => c.splitedItems.Count > 1).Select(c => c.splitedItems[1]).ToList();
+            var items2 = parsedItems.Where(c => c.splitedItems.Count > 2).Select(c => c.splitedItems[2]).ToList();
+
+            var res = new StringBuilder();
+            res.Append(items0.GroupBy(c => c).OrderByDescending(c => c.Count()).FirstOrDefault()?.Key);
+            var item1 = items1.GroupBy(c => c).OrderByDescending(c => c.Count()).FirstOrDefault(c => c.Count() > 1)?.Key;
+            if (!string.IsNullOrEmpty(item1))
             {
-
-                MessageBox.Show("We have Error."+ Environment.NewLine + ex.Message);
+                res.Append("." + item1);
             }
-            
-
+            var item2 = items2.GroupBy(c => c).OrderByDescending(c => c.Count()).FirstOrDefault(c => c.Count() > 1)?.Key;
+            if (!string.IsNullOrEmpty(item2))
+            {
+                res.Append("." + item2);
+            }
+            return res.ToString();
         }
 
         string[] GetAllDirectory(string path)
@@ -96,12 +104,14 @@ namespace ProjectRename
             {
                 var files = GetAllFiles(basePath);
 
+                var validFiles = files.Where(c => (new FileInfo(c)).Length < 20 * 1000 * 1000).ToList();
+
                 Invoke(new Action(() =>
                 {
-                    progress.Maximum = files.Length;
+                    progress.Maximum = validFiles.Count;
                     progress.Value = 0;
-                })) ;
-                foreach (var item in files)
+                }));
+                foreach (var item in validFiles)
                 {
                     Invoke(new Action(() => progress.Value += 1));
                     var newItem = item.Replace(basePath, newPath);
@@ -111,6 +121,42 @@ namespace ProjectRename
                     File.WriteAllText(newItem, content);
                 }
             });
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(txtFrom.Text) || String.IsNullOrEmpty(txtTo.Text) || String.IsNullOrEmpty(txtPath.Text))
+                {
+                    MessageBox.Show(@"Please select folder and then Set From and To texts");
+                }
+                else
+                {
+
+                    string from = txtFrom.Text;
+                    string to = txtTo.Text;
+                    string selectedFolder = txtPath.Text;
+                    string parent = Directory.GetParent(selectedFolder)?.ToString();
+                    string newFolder = parent + (string.IsNullOrEmpty(txtSolutionTo.Text) ? $"\\New-For-{to}" : "\\" + txtSolutionTo.Text);
+                    await CreateDirectoryStructure(newFolder, selectedFolder, from, to);
+
+                    await CopyAndReplace(newFolder, selectedFolder, from, to);
+
+                    var oldSolutionFile = newFolder + txtSolutionFrom + ".sln";
+                    if (File.Exists(oldSolutionFile))
+                    {
+                        System.IO.File.Move(oldSolutionFile, newFolder + txtSolutionTo+ ".sln");
+                    }
+
+                    MessageBox.Show(@"All Done.");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("We have Error." + Environment.NewLine + ex.Message);
+            }
         }
     }
 }
